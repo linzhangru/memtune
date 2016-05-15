573b20a106ec97791f6512830858e22b149b6df6
linzhangru <linzhangru@126.com>
2016-05-15 17:54:54 +0800

    raugh code for getting free memory status

--- main.c
+++ main.c
@@ -18,6 +18,18 @@ struct entry
 
 #define SIZE_MEMINFO 45
 #define INVALID_VAL  (-1)
+
+enum {
+    MEMTOTAL,
+    MEMFREE,
+    MEMAVAILABLE,
+    BUFFERS,
+    CACHED,
+    SWAPCACHED
+};
+
+    
+
 struct entry meminfo[SIZE_MEMINFO] = {
     {"MemTotal:",          INVALID_VAL},
     {"MemFree:",           INVALID_VAL},
@@ -87,7 +99,7 @@ int parse_meminfo()
 	printf("fail to open /proc/meminfo!\n");
 	return -1;
     }
-    printf("fd:%d\n", fd);
+    //printf("fd:%d\n", fd);
     if(read(fd, data, 4096) <= 0){
 	printf("/proc/meminfo read fail\n");
 	return -1;
@@ -104,7 +116,7 @@ int parse_meminfo()
 	    return -1;
 	}
 	sscanf(pline+strlen(meminfo[i].name), "%ld kB.*", &meminfo[i].val);
-	printf("%s %ld\n", meminfo[i].name, meminfo[i].val);
+	//printf("%s %ld\n", meminfo[i].name, meminfo[i].val);
     }
 
     //printf("%s\n", data);
@@ -117,7 +129,13 @@ int parse_meminfo()
 
 int main()
 {
-    parse_meminfo();
-
+    unsigned long free;
+    while(1){	
+	parse_meminfo();
+	free = meminfo[MEMFREE].val + meminfo[CACHED].val;
+	printf("free: %ld(F) + %ld(C) = %ld MB\n", meminfo[MEMFREE].val/1024, meminfo[CACHED].val/1024, free/1024);
+	sleep(1);
+    }
+    
     return 0;
 }
