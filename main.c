0099fc8358068d5c65c1580bc70f1ada5e28f501
linzhangru <linzhangru@126.com>
2016-05-15 22:36:33 +0800

    add vm config injection code

--- main.c
+++ main.c
@@ -137,11 +137,29 @@ enum {
 
 
 
+/*
+/proc/sys/vm/dirty_expire_centisecs:    3000 -->  2000 -->  1000
+/proc/sys/vm/dirty_writeback_centisecs:  500 -->   400 -->   300
+/proc/sys/vm/dirty_ratio:                 20 -->    15 -->    10
+/proc/sys/vm/dirty_background_ratio:      10 -->    20 -->    30 
+/proc/sys/vm/min_free_kbytes:           5624 --> 28120 --> 56240 
+/proc/sys/vm/vfs_cache_pressure:         100 -->   150 -->   200
+/proc/sys/vm/swappiness:                  60 -->    40 -->    10
+*/
+
+long config_val[3][7] = {
+    {3000, 500, 20, 10,  5624, 100, 60},
+    {2000, 400, 15, 20, 28120, 150, 40},
+    {1000, 300, 10, 30, 56240, 200, 10},
+};
+
+    
+
 struct entry vmfds[NR_FDS] = {
     {"/proc/sys/vm/dirty_expire_centisecs",    -1},
     {"/proc/sys/vm/dirty_writeback_centisecs", -1},
     {"/proc/sys/vm/dirty_ratio",               -1},
-    {"/proc/sys/vm/dirty_background_ratio",   -1},
+    {"/proc/sys/vm/dirty_background_ratio",    -1},
     {"/proc/sys/vm/min_free_kbytes",           -1},
     {"/proc/sys/vm/vfs_cache_pressure",        -1},
     {"/proc/sys/vm/swappiness",                -1}
@@ -149,16 +167,17 @@ struct entry vmfds[NR_FDS] = {
 
 
 
-int selet_vm_data()
+int select_vm_data()
 {
     //TBD: select vm_data according to the free memory status
+    return 0;
 }
 
 
 int get_vm_data(struct entry vmfds[])
 {
     char data[64];
-    unsigned long val;
+    long val;
 
     for(int i; i < NR_FDS; i++){
 	vmfds[i].val = open(vmfds[i].name, O_RDWR);
@@ -186,26 +205,62 @@ int get_vm_data(struct entry vmfds[])
 	if(vmfds[i].val >= 0)
 	    close(vmfds[i].val);
     }
-
-
+    printf("all vm files closed\n");
 }
 
 
 
-int reconfig_vmfds()
+int reconfig_vmfds(long * newvals)
 {
+    char data[64];
+    long val;
+    
     //TBD: inject new config datas through sysctl.vm
+    for(int i; i < NR_FDS; i++){
+	vmfds[i].val = open(vmfds[i].name, O_RDWR);
+	if(vmfds[i].val < 0){
+	    printf("fail to open file %s: %ld\n", vmfds[i].name, vmfds[i].val);
+	    return -1;
+	}
+	//printf("%s:%ld\n", vmfds[i].name, vmfds[i].val);
+    }
+    printf("all vm files open succeeded\n");
+
+
+    for(int i; i < NR_FDS; i++){
+	memset(data, 0, 64);
+	snprintf(data, sizeof(newvals[i]), "%ld\n", newvals[i]);
+	if(write(vmfds[i].val, data, sizeof(data)) <= 0){
+	    printf("file %s writes failure\n", vmfds[i].name);
+	    return -1;
+	}
+	//printf("_%s: %ld\n", vmfds[i].name, val);
+    }    
+    
+
+    for(int i; i < NR_FDS; i++){    
+	if(vmfds[i].val >= 0)
+	    close(vmfds[i].val);
+    }
+    printf("all vm files closed\n");
+    
 }
 
 
 int main()
 {
     unsigned long free;
+    int choice;
+    int sec = 0;
     
     while(1){
+	printf("============================\n"
+	       "seconds:%d, choice:%d\n", sec++, choice);
 	parse_meminfo();
 	free = meminfo[MEMFREE].val + meminfo[CACHED].val;
 	printf("free: %ld(F) + %ld(C) = %ld MB\n", meminfo[MEMFREE].val/1024, meminfo[CACHED].val/1024, free/1024);
+	choice = select_vm_data();
+	reconfig_vmfds(config_val[choice]);    
 	get_vm_data(vmfds);
 	sleep(1);
     }
