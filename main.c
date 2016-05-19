3d55a9cdad6b57f0af955b2eef14fb8f4c58acbf
zhangru_lin <zhangru_lin@htc.com>
2016-05-19 17:33:46 +0800

    init io loading related code

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <string.h>
 
 
+unsigned long long get_uptime();
 
 
 struct entry
@@ -255,7 +256,7 @@ int reconfig_vmfds(long * newvals)
 //TBD: "/sys/block/sdb/stat" is to be replaced by  "/sys/block/mmcblk0"
 //     they have the same out put format:
 #define EMMC_DEV_STAT "/sys/block/sdb/stat"   //"/sys/block/mmcblk0"
-struct block_stat {
+struct io_stat {
     int num_ird;    //1.number of issued reads
     int num_rdm;    //2.number of reads merged
     int num_scr;    //3.number of sectors read
@@ -267,6 +268,7 @@ struct block_stat {
     int num_ioc;    //9.number of I/Os currently in progress.
     int num_cms;    //10.number of milliseconds spent doing I/Os.
     int num_wms;    //11.number of milliseconds spent doing I/Os, weighted.
+    unsigned long long stp;  //time stamp of io_stat. unit: jiffy
 };
 
 
@@ -300,12 +302,14 @@ struct io_stats {
 #endif                                                                                                                                                                                
 
 
-struct block_stat blkst;
+struct io_stat blkst[2];
 
-int get_emmc_stat()
+int get_emmc_stat(int i)
 {
     int fd;
     char data[128];
+
+    blkst[i].stp = get_uptime();
     
     fd = open(EMMC_DEV_STAT, O_RDONLY);
     if(fd < 0){
@@ -318,19 +322,19 @@ int get_emmc_stat()
 	close(fd);
 	return -1;
     }
-    sscanf(data, "%d %d %d %d %d %d %d %d %d %d %d", &blkst.num_ird,
-	   &blkst.num_rdm, &blkst.num_scr, &blkst.num_msr, &blkst.num_wcp, &blkst.num_mrw,
-	   &blkst.num_scw, &blkst.num_msw, &blkst.num_ioc, &blkst.num_cms, &blkst.num_wms);
-    printf("%d %d %d %d %d %d %d %d %d %d %d\n", blkst.num_ird,
-	   blkst.num_rdm, blkst.num_scr, blkst.num_msr, blkst.num_wcp, blkst.num_mrw,
-	   blkst.num_scw, blkst.num_msw, blkst.num_ioc, blkst.num_cms, blkst.num_wms);
+    sscanf(data, "%d %d %d %d %d %d %d %d %d %d %d", &blkst[i].num_ird,
+	   &blkst[i].num_rdm, &blkst[i].num_scr, &blkst[i].num_msr, &blkst[i].num_wcp, &blkst[i].num_mrw,
+	   &blkst[i].num_scw, &blkst[i].num_msw, &blkst[i].num_ioc, &blkst[i].num_cms, &blkst[i].num_wms);
+    printf("%d %d %d %d %d %d %d %d %d %d %d\n", blkst[i].num_ird,
+	   blkst[i].num_rdm, blkst[i].num_scr, blkst[i].num_msr, blkst[i].num_wcp, blkst[i].num_mrw,
+	   blkst[i].num_scw, blkst[i].num_msw, blkst[i].num_ioc, blkst[i].num_cms, blkst[i].num_wms);
     
     close(fd);
     return 0;
 }
 
 #define HZ hz
-long int hz = 0;
+unsigned long int hz = 0;
 //get the number of ticks per second
 void get_hz(void)
 {
@@ -343,14 +347,13 @@ void get_hz(void)
     
 
 #define UPTIME "/proc/uptime"
-unsigned long uptime;
-
-int get_uptime()
+//the return val is in unit of jiffies
+unsigned long long get_uptime()
 {
     int fd;
     char data[64];
-    unsigned long up_sec, up_csec;
-
+    unsigned long long up_sec, up_csec;
+    unsigned long long uptime;
     if(!hz){
 	printf("HZ val not yet initialized\n");
 	return -1;
@@ -365,13 +368,71 @@ int get_uptime()
 	printf("read %s failure\n", UPTIME);
 	return -1;
     }
-    sscanf(data, "%lu.%lu", &up_sec, &up_csec);
+    sscanf(data, "%llu.%llu", &up_sec, &up_csec);
     uptime = up_sec*HZ + up_csec*HZ/100;
     
-    printf("%s, up_sec:%ld, up_csec:%ld, uptime:%ld, HZ:%ld\n", data, up_sec, up_csec, uptime, HZ);
-
+    printf("%s, up_sec:%lld, up_csec:%lld, uptime:%lld, HZ:%ld\n", data, up_sec, up_csec, uptime, HZ);
     close(fd);
+
+    return uptime;
 }
+
+
+
+
+
+//return current io_stat index and update the current index
+//to the next for the next time get_cur() called
+static int cur = 0;
+int get_cur()
+{
+    return cur;
+}
+
+void switch_cur(){
+    cur = (cur+1)%2; 
+}
+
+#define LOADING_RD_THRES  200
+#define LOADING_WR_THRES  200
+
+#define FCTR 2048;
+    
+
+int get_io_loading()
+{
+    int i, j;
+    double ldr, ldw;
+    int interval;
+    
+    switch_cur();
+    get_emmc_stat(get_cur());
+    i = get_cur();
+    j = (i+1)%2;
+    if(!(blkst[i].stp - blkst[j].stp)){
+	interval = 1;
+    } else {
+	interval = blkst[i].stp - blkst[j].stp;
+    }
+
+    printf("%d: %d,%d\n", i, blkst[i].num_scr, blkst[i].num_scw);
+    printf("%d: %d,%d\n", j, blkst[j].num_scr, blkst[j].num_scw);
+
+    printf("interval:%d\n", interval);
+    
+    ldr = (double)((blkst[i].num_scr - blkst[j].num_scr)*HZ)/interval/FCTR;
+    ldw = (double)((blkst[i].num_scw - blkst[j].num_scw)*HZ)/interval/FCTR;
+
+    printf("ldr:%8.2f, ldw:%8.2f\n", ldr, ldw);
+    
+    //TBD: current algorithm of deciding high loading is the be improved
+    if(ldr > LOADING_RD_THRES || ldw > LOADING_WR_THRES) {
+	return 1;   //loading high
+    } else {
+	return 0;   //loading normal
+    }
+}
+
 
 
 
@@ -379,20 +440,31 @@ int select_vm_data(int free)
 {
     //TBD: select vm_data according to the free memory status
     //     and eMMC loading ==> not yet finished
-
-    get_emmc_stat();
-    
+    int io_loading_high;
     int thres;
+
+    io_loading_high = get_io_loading();
+    
     if(free/1024 > THRESHOLD_0)
 	return 0;
-    else if(free/1024 > THRESHOLD_1)
+    //trigger page reclaim when io loading is not high
+    else if((free/1024 > THRESHOLD_1) && !io_loading_high)
 	return 1;
-    else if(free/1024 > THRESHOLD_2)
+    //trigger page reclaim when io loading is not high
+    else if((free/1024 > THRESHOLD_2) && !io_loading_high)
 	return 2;
     else
 	return 0;  //TBD: consider about more vm configration choices
 }
 
+
+
+
+int init()
+{
+    get_hz();
+    get_emmc_stat(get_cur());
+}
 
 
 
@@ -402,14 +474,12 @@ int main()
     int choice;
     int sec = 0;
 
-    get_hz();
-    get_uptime();
 
+    init();
+    sleep(1);
     
-    //get_emmc_stat();
-    
-#if 0
-    while(1){
+#if 1
+    while(1) {
 	printf("============================\n"
 	       "seconds:%d, choice:%d\n", sec++, choice);
 	parse_meminfo();
@@ -419,6 +489,7 @@ int main()
 	reconfig_vmfds(config_val[choice]);    
 	get_vm_data(vmfds);
 	sleep(1);
+	
     }
 #endif    
     return 0;
